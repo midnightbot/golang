d4c30606759a3a752d3158c44074a45caea37a06
Anish Adnani <44056065+midnightbot@users.noreply.github.com>
2023-05-07 13:35:58 -0700

    go-functions

--- project1/hello.go
+++ project1/hello.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -47,4 +49,52 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
+
+	// while loop in go
+	y := 0
+	for y < 5 {
+		fmt.Println(y)
+		y++
+	}
+
+	// iterating over array
+	arr1 := []string{"a", "b", "c"}
+
+	for index, value := range arr1 {
+		fmt.Println("index", index, value)
+	}
+
+	// iterating over map
+	ms := make(map[string]string)
+	ms["a"] = "anish"
+	ms["r"] = "rohan"
+
+	for key, value := range ms {
+		fmt.Println("Key", key, "Value : ", value)
+	}
+
+	// calling functions
+	result := sum(2, 3)
+	fmt.Println(result)
+
+	result0, err := sqrt(-16)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result0)
+	}
 }
+
+func sum(x int, y int) int {
+	return x + y
+}
+
+// functions can return multiple things
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undefined Negative Number")
+	}
+	return math.Sqrt(x), nil
+}
+
+// go does not have exceptions
